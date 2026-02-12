a4fb0ec3d3964ed44ca918e3fef248815d078955
Yocode5 <155065982+Yocode5@users.noreply.github.com>
2026-02-13 03:21:43 +0530

    Update : Added the Login API route and DB Operations , and a Test Data Creator

--- routes/passenger.js
+++ routes/passenger.js
@@ -1,12 +1,56 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 const Bus = require('../models/Bus');   
+const Passenger = require('../models/Passenger');
 
 // Route to get the latest location of the buses
 router.get('/locations', async (req, res) => {
     try {
         const buses = await Bus.getActiveBuses();
         res.json(buses);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Server Error' });
+    }
+})
+
+//Passenger Login Route
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const user = await Passenger.findByUsername(username);
+
+        if (!user) {
+            return res.status(401).json({ message: 'User not found' });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password_hash);
+
+        if (!isMatch) {
+            return res.status(401).json({ message: 'Invalid credentials' });
+        }
+
+        const token = jwt.sign(
+            {id: user.passenger_id, username: user.username}, 
+            process.env.JWT_SECRET || 'default_secret_key',
+            { expiresIn: '7d' }
+        );
+
+        res.json({
+            message: 'Login successful',
+            token,
+            user: {
+                id: user.passenger_id,
+                username: user.username,
+                first_name: user.first_name,
+                last_name: user.last_name,
+                balance: user.balance
+            }
+        });
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Server Error' });
