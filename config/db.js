27c2760d7bc64e79c6d6bd1fb42a1aa0b02da724
Yocode5 <155065982+Yocode5@users.noreply.github.com>
2026-03-12 12:51:09 +0530

    fix: correct database user environment variable in connection pool configuration

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const db = mysql.createPool({
     connectionLimit: 10,
     host: process.env.HOST,
-    user: process.env.USER,
+    user: process.env.DB_USER,
     password: process.env.PASSWORD,
     database: process.env.DB_NAME,
 
