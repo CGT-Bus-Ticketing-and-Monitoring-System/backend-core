0fe24777bd5036e22b83384ee3d1cee06fbb71c4
Manuja Demin <mmdwijesinghe@students.nsbm.ac.lk>
2026-03-16 17:34:58 +0530

    Route BACKEND v2

--- models/Route.js
+++ models/Route.js
@@ -103,6 +103,19 @@ class Route {
         const [rows] = await db.promise().execute("SELECT route_id, route_code FROM Route WHERE status = 'ACTIVE'");
         return rows;
     }
+
+    static async getAllBusRoutes(){
+        return new Promise((resolve , reject) => {
+            const sql_bus = `
+            SELECT route_code , start_location , end_location
+            FROM Route WHERE status = 'ACTIVE'
+            `;
+            db.query(sql_bus , (err,results)=> {
+                if(err) return reject(err);
+                resolve(results);
+            });
+        });
+    }
 }
 
 module.exports = Route;
